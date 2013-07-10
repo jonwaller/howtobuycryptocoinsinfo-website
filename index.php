d90d0559720cd9a7a3ddeb3595c452362c807905
Jonathan WALLER <public@jonwaller.net>
2013-07-11 00:28:59 +0900

    Added the warnings box in 4 more languages.

--- index.php
+++ index.php
@@ -77,15 +77,54 @@ $currentcountry = $_REQUEST['country'];
 
       <div id="warningarea">
         <div class="warningBox">
-          <h3 class="box-title">Warning: Please be careful with your money.</h3>
+          <h3 class="box-title">Warning: Please be careful with your money. 
+            <img src="img/miniflags/us.png" onclick="showLang('en')"> 
+            <img src="img/miniflags/cn.png" onclick="showLang('cn')"> 
+            <img src="img/miniflags/es.png" onclick="showLang('es')"> 
+            <img src="img/miniflags/jp.png" onclick="showLang('jp')"> 
+            <img src="img/miniflags/fr.png" onclick="showLang('fr')">
+          </h3>
           <div class="box-content">
             <p>
-              <small>
-              When sending money to an exchange, you are trusting the operator to not steal your funds, and that their site is secure. <br/><br/>
-              It is recommended you obtain the real-world identity of the operator and ensure that sufficient recourse is available.<br/>
-              Exchanging or storing significant amounts of funds with third-parties is not recommended.<br/><br/>
-              Bitcoin services are not highly regulated so a service can continue operating even when it is widely believed that it is insecure or dishonest. Also, webpages recommending them (such as this one) may not be regularly updated. (However, saying that, the site is <a href="https://github.com/jonwaller/howtobuybitcoinsinfo-website">open-source</a>, and I try to respond quickly to <a href="mailto:info@howtobuybitcoins.info">emails</a>.)
+              <small class="langBox en">
+                When sending money to an exchange, you are trusting the operator to not steal your funds, and that their site is secure. <br/><br/>
+                It is recommended you obtain the real-world identity of the operator and ensure that sufficient recourse is available.<br/>
+                Exchanging or storing significant amounts of funds with third-parties is not recommended.<br/><br/>
+                Bitcoin services are not highly regulated so a service can continue operating even when it is widely believed that it is insecure or dishonest. Also, webpages recommending them (such as this one) may not be regularly updated. (However, saying that, the site is <a href="https://github.com/jonwaller/howtobuybitcoinsinfo-website">open-source</a>, and I try to respond quickly to <a href="mailto:info@howtobuybitcoins.info">emails</a>.)
               </small>
+              <small class="langBox cn">
+                向交易所汇款时，要相信操作员不会盗取您的资金，而且交易所的网站很安全。
+                <br/><br/>
+                建议了解操作员的真实身份，并确保有足够的追索权。<br/>
+                不建议向第三方兑换或储存大量资金。
+                <br/><br/>
+                对比特币服务的管控不是很严，因此即使人们广泛认为某种服务不安全或不可靠，这种服务也可以继续运行下去。此外，从中推荐这些服务的网页（如本网页）也可能未得到定期更新。（虽说如此，但此网站<a href="https://github.com/jonwaller/howtobuybitcoinsinfo-website">是开源</a>网站，而且我也尽快<a href="mailto:info@howtobuybitcoins.info">对电子邮件</a>作出答复。）
+              </small>
+              <small class="langBox es">
+                Al enviar dinero para un intercambio, estás confiando en que el agente no roba tus fondos y que su sitio web es seguro.
+                <br/><br/>
+                Se recomienda que obtengas la identidad real del agente y te asegures que dispone de los recursos suficientes.<br/>
+                No es recomendable intercambiar o depositar cantidades importantes de dinero con terceras partes.
+                <br/><br/>
+                Los servicios Bitcoin no están excesivamente regulados, por lo que un servicio puede continuar operando aún cuando es de sobra conocido que es inseguro o fraudulento. Además, las páginas web que los recomiendan (como esta) puede que no se actualicen con frecuencia (sin embargo, y una vez dicho esto, el sitio es <a href="https://github.com/jonwaller/howtobuybitcoinsinfo-website">de código abierto</a> e intento responder rápidamente a los <a href="mailto:info@howtobuybitcoins.info">correos electrónicos</a>).
+              </small>
+              <small class="langBox jp">
+                取引所に送金する場合、貴方はオペレーターが貴方の資金を盗まないということ、そしてサイトが安全であるということを信用するということになります。
+                <br/><br/>
+                オペレーターの実世界での情報と充分に遡及が可能であることを確認することが推奨されます。<br/>
+                高額な資金を第三者と交換する、あるいは第三者に預けることは推奨されません。
+                <br/><br/>
+                ビットコインの関連サービスには厳しい規制がなく、サービスが信頼できない、あるい不当なものだと広く考えられていても営業を続けることができます。また、それらを推奨するウェブサイト（このサイトもその一例です）は頻繁にアップデートされていない場合があります。（しかし、そうは言ってもこのサイトは<a href="https://github.com/jonwaller/howtobuybitcoinsinfo-website">オープンソース</a>であり、<a href="mailto:info@howtobuybitcoins.info">メール</a>には迅速に返答する努力をしています。）
+              </small>
+              <small class="langBox fr">
+                Lorsque vous envoyez de l'argent pour un échange, vous faites confiance au fait que l'opérateur ne va pas voler vos fonds, et que son site est sécurisé.
+                <br/><br/>
+                Il est recommandé que vous obteniez la véritable identité de l'opérateur et que vous vous assuriez que des recours suffisants sont disponibles.<br/>
+                Échanger ou stocker des sommes importantes auprès de tiers n'est pas recommandé.
+                <br/><br/>
+                Les services bitcoins ne sont pas hautement régulés, donc un service peut continuer à fonctionner même lorsqu'il est largement admis qu'il n'est pas sécurisé ou malhonnête. En outre, les pages web les recommandant (comme celle-ci) peuvent ne pas être mises à jour régulièrement. (Cependant, je précise que ce site est <a href="https://github.com/jonwaller/howtobuybitcoinsinfo-website">open-source</a>, et que j'essaie de répondre rapidement aux <a href="mailto:info@howtobuybitcoins.info">emails</a>.)
+              </small>
+
             </p>
           </div>
         </div>
@@ -137,7 +176,28 @@ $currentcountry = $_REQUEST['country'];
     .appendTo(ul);
   };
 
+  function showLang(langCode){
+    $(".langBox").hide();
+    $(".langBox."+langCode).show();
+  }
+
   $(document).ready(function(){
+
+    var currentCountryCode="<?=$currentcountry?>";
+
+    $(".langBox").hide();
+    
+    if (currentCountryCode=="jp") {
+      $(".langBox.jp").show();
+    }else if (currentCountryCode=="cn") {
+      $(".langBox.cn").show();
+    }else if (currentCountryCode=="es") {
+      $(".langBox.es").show();
+    }else if (currentCountryCode=="fr") {
+      $(".langBox.fr").show();
+    }else{      
+      $(".langBox.en").show();
+    }
 
     var countries = [];
     $("#warningarea").masonry({itemSelector:".warningBox"});
